fc7ec5bdf466eeea7da2a78e6b1ae2de5b700302
Sheila1009 <122435673+Sheila1009@users.noreply.github.com>
2023-05-03 18:54:20 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const parrafo = (document.querySelector(
+    "#excuse"
+  ).innerHTML = excusaGenerator());
 };
+//write your code here
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excusaGenerator() {
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  let excusa =
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+  return excusa;
+}
